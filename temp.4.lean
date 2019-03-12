797ebfac3a9d7b7314b68950a9ba938eae7770a6
Kevin Sullivan <sullivan.kevinj@gmail.com>
2019-03-12 12:12:16 -0400

    phys deltas

--- temp.4.lean
+++ temp.4.lean
@@ -2,8 +2,7 @@ import vec
 
 def time : peirce.space := peirce.space.mk 0
 def geom : peirce.space := peirce.space.mk 1
-def v1 : vec geom := ( peirce.vec.mkVector geom 0.000000 0.000000 0.000000 )
-def v2 : vec geom := ( peirce.vec.mkVector geom 0.000000 0.000000 0.000000 )
-def v3 : vec geom := ( add ( ( add ( v2 : vector geom ) ( v1 : vector geom ) : g
-eom ) ) : geom ) ( add ( v1 : vector geom ) ( v2 : vector geom ) : geom ) : geom
- )
+def v1 : peirce.vec geom := ( peirce.vec.mkVector geom 0.000000 0.000000 0.000000 )
+def v2 : peirce.vec geom := ( peirce.vec.mkVector geom 0.000000 0.000000 0.000000 )
+def v3 : peirce.vec geom := ( peirce.add ( ( peirce.add ( v2 : peirce.vec geom ) ( v1 : peirce.vec geom ) : peirce.space geom ) ) : peirce.space geom ) ( peirce.add ( v1 : peirce.vec geom ) ( v2 : peirce.vec geom ) : peirce.space geom ) : peirce.space geom )
+
