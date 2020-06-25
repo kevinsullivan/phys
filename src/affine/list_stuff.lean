1df03fe7e6dc9262b6d375a892aa96592b6e3f6f
ukdl123 <31701136+ukdl123@users.noreply.github.com>
2020-06-25 06:19:03 -0400

    added necessary lemmata
    - removed unnecessary imports
    - added 4 crucial theorems: add_cons_cons, add_nil_left, add_nil_right, length_sum
    - added various trivial lemmata that are important for proofs in aff_coord_space

--- src/affine/list_stuff.lean
+++ src/affine/list_stuff.lean
@@ -1,12 +1,46 @@
-import data.list.zip
-
 open list
 
-/-- Implements list addition coordinate-wise w.r.t. the addition function on the list entries. -/
-instance (α : Type*) [has_add α] : has_add (list α) := ⟨zip_with has_add.add⟩
+universes u v
+variables (K : Type u) [field K] {α : Type v} [has_add α]
+(x : K) (xl : list K)
 
-universe u
-variables (K : Type u) [field K]
+/-- addition function on `list α`, where `α` has an addition function -/
+def ladd : list α → list α → list α := zip_with has_add.add
+
+/- 
+    The following theorems are stolen from data.list.zip and adapted for the zip_with function, which is
+    more generic than the zip function the lemmata were initially used for. These lemmata are used in the
+    file aff_coord_space to prove statements necessary to show aff_vec is a vector space. 
+-/
+
+/-- addition is compatible with list constructor -/
+@[simp] theorem add_cons_cons (a b : α) (l₁ l₂ : list α) :
+  ladd (a :: l₁) (b :: l₂) = (a + b) :: ladd l₁ l₂ := rfl
+
+/-- adding the empty list to a list gives you the empty list -/
+@[simp] theorem add_nil_left (l : list α) : ladd ([] : list α) l = [] := rfl
+
+/-- adding a list to the empty list gives you the empty list -/
+@[simp] theorem add_nil_right (l : list α) : ladd l ([] : list α) = [] :=
+by cases l; refl
+
+/-- The length of the sum of two lists is the length of the shorter list -/
+@[simp] theorem length_sum : ∀ (l₁ : list α) (l₂ : list α),
+   length (ladd l₁ l₂) = min (length l₁) (length l₂)
+| []      l₂      := rfl
+| l₁      []      := by simp -- TODO: figure out which simp lemmata are being used, and use "simp only"
+| (a::l₁) (b::l₂) := by simp only [length, add_cons_cons, length_sum l₁ l₂, min_add_add_right]
+
+/-- the length of nil is 0 -/
+lemma nil_len : length ([] : list α) = 0 := rfl
+
+/-- Implements list addition coordinate-wise w.r.t. the addition function on the list entries. -/
+instance (α : Type*) [has_add α] : has_add (list α) := ⟨ladd⟩
+
+-- TODO: rename this lemma. It's actually important
+lemma sum_test : ∀ a b : list K, a + b = ladd a b := by {intros, refl}
+
+lemma sum_test' : ∀ a b : list K, a + b = (zip_with has_add.add) a b := by {intros, refl}
 
 /-- returns list rep of 0 vector of given length. -/
 def list.field_zero : ℕ → list K
@@ -19,5 +53,14 @@ def list.field_scalar : K → list K → list K
 | x (a :: l) := (x * a) :: (list.field_scalar x l)
 
 
--- TODO: implement list negation
-#check map
+def field_neg : K → K := λ a, -a
+
+def list.neg : list K → list K := map (field_neg K)
+
+def list.neg' : list K → list K := λ x, list.field_scalar K (-1 : K) x
+
+def list.neg'' : list K → list K
+| [] := []
+| (x :: xl) := (-x) :: list.neg'' xl
+
+lemma neg_cons : list.neg K (x :: xl) = (-x) :: list.neg K xl := rfl
