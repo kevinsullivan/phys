6a36f29b83f5ed99b11c7960f6bcdfe5b1d9420b
drewjel <andrew.james.elsey@gmail.com>
2020-10-28 01:43:15 -0400

    Pushing time changes

--- src/classical_time.lean
+++ src/classical_time.lean
@@ -1,14 +1,56 @@
 import .....math.new_affine.real_affine_space
 import ..metrology.dimension 
+import ..metrology.measurement
 
 structure classicalTime : Type :=
 mk :: (name : ℕ) -- name serves as unique ID for a given geometric space
 
+
+structure classicalTimeVector (t : classicalTime) : Type :=
+mk :: (name : ℕ) (val : vector ℝ 1)
+
+structure classicalTimePoint (t : classicalTime) : Type :=
+mk :: (name : ℕ) (val : vector ℝ 1)
+
+--inductive classicalTimeFrame : classicalTime → Type 
+--| standard (t : classicalTime) : classicalTimeFrame t
+--| derived (t : classicalTime) : classicalTimeFrame t
+
+open measurementSystem
+
+inductive classicalTimeFrame : classicalTime → Type 
+| standard (t : classicalTime) (m : MeasurementSystem) : classicalTimeFrame t
+| derived (t : classicalTime) : 
+    classicalTimeFrame t → classicalTimePoint t → (fin 1 → classicalTimeVector t) → MeasurementSystem → classicalTimeFrame t
+
+structure classicalTimeCoordinateVector (s : classicalTime) extends classicalTimeVector s :=
+(f : classicalTimeFrame s)--mk :: (name : ℕ) (val : vector ℝ 1) {s : classicalTime} (f : classicalTimeFrame s)
+
+structure classicalTimeCoordinatePoint (s : classicalTime) extends classicalTimePoint s :=
+(f : classicalTimeFrame s)--mk 
 -- provide standard world time object
 def worldTime := classicalTime.mk 0 
 
 noncomputable def classicalTimeAlgebra : classicalTime → real_affine.Algebra
-| (classicalTime.mk n) := real_affine.Algebra.aff_space (real_affine.to_affine 1)
+| (classicalTime.mk n) := real_affine.Algebra.aff_space (real_affine.to_affine_space 1)
+
+noncomputable def classicalTimeVectorAlgebra {t : classicalTime} : classicalTimeVector t → real_affine.Algebra
+| (classicalTimeVector.mk _ v) := real_affine.Algebra.aff_vector (real_affine.to_affine_vector v)
+
+noncomputable def classicalTimePointAlgebra {t : classicalTime} : classicalTimeVector t → real_affine.Algebra
+| (classicalTimeVector.mk _ v) := real_affine.Algebra.aff_point (real_affine.to_affine_point v)
+
+noncomputable def classicalTimeFrameAlgebra {t : classicalTime} : classicalTimeFrame t → real_affine.r_fr 1
+| (@classicalTimeFrame.standard t m) := real_affine.to_standard_frame 1
+| (@classicalTimeFrame.derived t f p v m) := real_affine.to_derived_frame 1 p.val (λn : fin 1, (v n).val) (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra f))
+
+
+noncomputable def classicalTimeCoordinateVectorAlgebra {t : classicalTime} : classicalTimeCoordinateVector t → real_affine.Algebra :=
+    λ v, real_affine.Algebra.aff_vector (real_affine.to_affine_vector_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra v.f)))
+
+noncomputable def classicalTimeCoordinatePointAlgebra {t : classicalTime} : classicalTimeCoordinatePoint t → real_affine.Algebra :=
+    λ v, real_affine.Algebra.aff_point (real_affine.to_affine_point_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra v.f)))
+
 
 -- Kevin: add has_to_algebra typeclass
 
---
+++ src/test/phys_test_1.lean
@@ -0,0 +1,6 @@
+/-
+    //1 : worldTime = ClassicalTime()
+    // si = SI()
+    //1 : timeFrame = worldTime.stdFrame() with si
+    //1 : timePoint = Point(worldTime,stdFrame,<5>, si_measurement_system)
+-/
