8aa2079d27a8584e0ebaa6b46eb90b3a8d04b825
drewjel <andrew.james.elsey@gmail.com>
2020-10-29 15:59:23 -0400

    Adjust algebra, add test examples

--- src/classical_time.lean
+++ src/classical_time.lean
@@ -40,16 +40,19 @@ noncomputable def classicalTimeVectorAlgebra {t : classicalTime} : classicalTime
 noncomputable def classicalTimePointAlgebra {t : classicalTime} : classicalTimeVector t → real_affine.Algebra
 | (classicalTimeVector.mk _ v) := real_affine.Algebra.aff_point (real_affine.to_affine_point v)
 
-noncomputable def classicalTimeFrameAlgebra {t : classicalTime} : classicalTimeFrame t → real_affine.r_fr 1
+noncomputable def classicalTimeFrameHelper  {t : classicalTime} : classicalTimeFrame t → real_affine.real_affine_frame 1
 | (@classicalTimeFrame.standard t m) := real_affine.to_standard_frame 1
-| (@classicalTimeFrame.derived t f p v m) := real_affine.to_derived_frame 1 p.val (λn : fin 1, (v n).val) (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra f))
+| (@classicalTimeFrame.derived t f p v m) := real_affine.to_derived_frame 1 p.val 
+    (λn : fin 1, (v n).val) (real_affine.to_coordinatized_affine_space 1 
+    (classicalTimeFrameHelper f).1)
 
-
+noncomputable def classicalTimeFrameAlgebra {t : classicalTime} : classicalTimeFrame t → real_affine.Algebra
+| f := real_affine.Algebra.aff_frame (classicalTimeFrameHelper f)
 noncomputable def classicalTimeCoordinateVectorAlgebra {t : classicalTime} : classicalTimeCoordinateVector t → real_affine.Algebra :=
-    λ v, real_affine.Algebra.aff_vector (real_affine.to_affine_vector_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra v.f)))
+    λ v, real_affine.Algebra.aff_vector (real_affine.to_affine_vector_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameHelper v.f).1))
 
 noncomputable def classicalTimeCoordinatePointAlgebra {t : classicalTime} : classicalTimeCoordinatePoint t → real_affine.Algebra :=
-    λ v, real_affine.Algebra.aff_point (real_affine.to_affine_point_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameAlgebra v.f)))
+    λ v, real_affine.Algebra.aff_point (real_affine.to_affine_point_with_frame v.val (real_affine.to_coordinatized_affine_space 1 (classicalTimeFrameHelper v.f).1))
 
 
 -- Kevin: add has_to_algebra typeclass
--- src/classical_time_test.lean
+++ src/classical_time_test.lean
@@ -1,3 +1,29 @@
 import .classical_time
 
 noncomputable def aTimeSpace := classicalTime.mk 0
+
+def si := measurementSystem.si_measurement_system
+
+noncomputable def my_standard_frame := 
+    classicalTimeFrame.standard aTimeSpace si
+
+noncomputable def my_standard_frame_algebra := classicalTimeFrameAlgebra my_standard_frame 
+
+
+noncomputable def my_vector : classicalTimeVector aTimeSpace :=
+    classicalTimeVector.mk  1 ⟨[2],sorry⟩
+
+noncomputable def std_vector : classicalTimeCoordinateVector aTimeSpace 
+    := {f:=my_standard_frame,..my_vector}
+
+noncomputable def my_Point : classicalTimePoint aTimeSpace :=
+    classicalTimePoint.mk  1 ⟨[0],sorry⟩
+
+
+noncomputable def std_point : classicalTimeCoordinatePoint aTimeSpace 
+    := {f:=my_standard_frame,..my_Point}
+
+
+noncomputable def my_derived_frame : classicalTimeFrame aTimeSpace :=
+    classicalTimeFrame.derived aTimeSpace my_standard_frame my_Point (λone : fin 1,my_vector) si
+
